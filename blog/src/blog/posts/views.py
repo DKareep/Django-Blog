5dc9051a602c964868b25d47c175ace8e6544471
Dijin <dijin@lookup.to>
2016-05-14 19:23:10 +0530

    post creation using forms

--- blog/src/blog/posts/views.py
+++ blog/src/blog/posts/views.py
@@ -2,15 +2,25 @@ from django.shortcuts import HttpResponse
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-
+from .forms import PostForm
 from models import Post
 
 
 def post_create(request):
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        print form.cleaned_data.get("titlec")
+        instance.save()
+
+    # if request.method == 'POST':
+    #     print request.POST.get('title')
+    #     print request.POST.get('content')
     context = {
-        "title": "create"
+        "title": "create",
+        "form": form
     }
-    return render(request, "index.html", context)
+    return render(request, "form_view.html", context)
 
 
 def post_detail(request, id):
